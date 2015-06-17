c0a8d996b8ecc72a254cb591dbc0c2d66894a512
Ayan Yenbekbay <ayan.yenb@gmail.com>
2015-06-17 11:08:38 +0600

    Initial Commit

---
+++ Day 4 Photo Bompers App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Day 4 Photo Bompers App
+//
+//  Created by Элина on 17.06.15.
+//  Copyright (c) 2015 Элина. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
