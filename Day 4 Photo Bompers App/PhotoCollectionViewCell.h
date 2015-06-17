969b479f36772c3a1a74da2697f33c14eb748970
Ayan Yenbekbay <ayan.yenb@gmail.com>
2015-06-17 12:37:44 +0600

    Collection View design created

---
+++ Day 4 Photo Bompers App/PhotoCollectionViewCell.h
@@ -0,0 +1,16 @@
+//
+//  PhotoCollectionViewCell.h
+//  Day 4 Photo Bompers App
+//
+//  Created by Элина on 17.06.15.
+//  Copyright (c) 2015 Элина. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoCollectionViewCell : UICollectionViewCell
+
+@property (nonatomic) UIImageView *imageView;
+
+
+@end
---
+++ Day 4 Photo Bompers App/PhotoCollectionViewCell.m
@@ -0,0 +1,31 @@
+//
+//  PhotoCollectionViewCell.m
+//  Day 4 Photo Bompers App
+//
+//  Created by Элина on 17.06.15.
+//  Copyright (c) 2015 Элина. All rights reserved.
+//
+
+#import "PhotoCollectionViewCell.h"
+
+@implementation PhotoCollectionViewCell
+
+-(instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    
+    if (self ){
+        self.imageView = [UIImageView new];
+        [self.contentView addSubview:self.imageView];
+    
+    }
+    
+    return self;
+}
+-(void)layoutSubviews
+{
+    [super layoutSubviews];
+    self.imageView.frame = self.contentView.bounds;
+}
+
+@end
